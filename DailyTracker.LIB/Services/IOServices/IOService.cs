f3bef793144938991585714025cefc6e20a6edcb
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2023-08-02 04:10:53 +0300

    Create IO services to accsess data

---
+++ DailyTracker.LIB/Services/IOServices/IOService.cs
@@ -0,0 +1,22 @@
+﻿namespace DailyTracker.LIB.Services.IOServices;
+
+public abstract class IOService<TObject>
+{
+    protected string DirectoryLocation { get; set; }
+
+    protected string FileName { get; set; } = string.Empty;
+
+    protected string FilePath => Path.Combine(DirectoryLocation, FileName);
+
+    public IOService(string directoryPath)
+    {
+        if (Directory.Exists(directoryPath) == false)
+            throw new DirectoryNotFoundException();
+
+        DirectoryLocation = directoryPath;
+    }
+
+    public abstract TObject? Read();
+
+    public abstract void Write(TObject? data);
+}
---
+++ DailyTracker.LIB/Services/IOServices/JsonIOService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace DailyTracker.LIB.Services.IOServices;
+
+public class JsonIOService<TObject> : IOService<TObject>
+{
+    public JsonIOService(string directoryPath, string fileName) : base(directoryPath)
+    {
+        FileName = fileName;
+
+        if (Path.GetExtension(FilePath) != ".json")
+            throw new FileLoadException();
+
+        if (File.Exists(FilePath) == false)
+            File.Create(FilePath);
+    }
+
+    public override TObject? Read()
+        => JsonConvert.DeserializeObject<TObject?>(File.ReadAllText(FilePath)) ?? throw new NullReferenceException();
+
+    public override void Write(TObject? data)
+        => File.WriteAllText(FilePath, JsonConvert.SerializeObject(data, Formatting.Indented));
+}
