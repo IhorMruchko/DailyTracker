e5311a7c8e9485466a2bdf300a40d5b676181a12
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2023-08-02 04:10:21 +0300

    Set up basic entites

---
+++ DailyTracker.LIB/Entities/DailyTask.cs
@@ -0,0 +1,14 @@
+﻿namespace DailyTracker.LIB.Entities;
+
+public class DailyTask
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public TimeOnly? Start { get; set; } = null;
+
+    public TimeOnly? Duration { get; set; } = null;
+
+    public bool IsDone { get; set; } = false;
+}
---
+++ DailyTracker.LIB/Entities/Day.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyTracker.LIB.Entities;
+
+public class Day
+{
+    public DayOfWeek DayName { get; set; }
+
+    public List<DailyTask> EveryDayRoutine { get; set; } = new List<DailyTask>();
+
+    public List<DailyTask> Tasks { get; set; } = new List<DailyTask>();
+}
